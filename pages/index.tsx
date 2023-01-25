ebb8680dbbc7c0066daa3541722cd6f17fc7f513
chaewoong0407 <an0304073@naver.com>
2023-01-25 16:25:06 +0900

    style 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
+import styled from '@emotion/styled';
+import Head from 'next/head';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
 const Home = () => {
-  return <div>hello</div>;
+  return (
+    <>
+      <Head>
+        <title>배구 기록 페이지</title>
+      </Head>
+      <Container>sss</Container>
+    </>
+  );
 };
 
 export default Home;
