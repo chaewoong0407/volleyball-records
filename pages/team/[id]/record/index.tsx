f802e7cedd58352e0fcf784f8631f73003482518
chaewoong0407 <an0304073@naver.com>
2023-02-02 23:07:23 +0900

    팀기록 페이지

---
+++ pages/team/[id]/record/index.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import { Header, SubNavgation, TeamDetails } from 'components';
+import { TokenClient } from 'lib/Axios';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { TeamIntroductionType } from '../introduction';
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: calc(100% - 140px);
+`;
+
+const Record = () => {
+  const router = useRouter();
+  const id = router.query.team_id;
+  const [team, setTeam] = useState<TeamIntroductionType>({
+    id: 0,
+    name: '',
+    team_logo: '',
+    gender: true,
+    created_at: '',
+    coach: '',
+    performance: [],
+  });
+
+  useEffect(() => {
+    TokenClient.get('/team/introduction', { params: { team_id: id } })
+      .then((response) => {
+        console.log(response.data);
+        console.log(response.status);
+
+        if (response.status === 200) {
+          setTeam(response.data.data);
+        }
+      })
+      .catch((response) => {
+        console.log(response.data);
+        console.log(response.status);
+      });
+  }, [id]);
+  return (
+    <Container>
+      <Header />
+      <SubNavgation />
+      <TeamDetails team={team} />
+    </Container>
+  );
+};
+
+export default Record;
