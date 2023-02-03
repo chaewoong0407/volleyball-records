c7e93803830edb90673daa2266b58dd3ea12c855
chaewoong0407 <an0304073@naver.com>
2023-02-03 15:26:23 +0900

    config 수정

--- lib/Axios.ts
+++ lib/Axios.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
-import { SERVER } from '../config/config.json';
+import SERVER from '../config/config.json';
 import { getToken } from './Storage';
 
 export const client = axios.create({
-  baseURL: SERVER,
+  baseURL: SERVER.SERVER,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export const TokenClient = axios.create({
-  baseURL: SERVER,
+  baseURL: SERVER.SERVER,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${getToken()}`,
